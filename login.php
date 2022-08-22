0246416c733f40a9cd4921875c80c7e9bf88edf2
unknown <rr478827@gmail.com>
2022-08-22 21:45:44 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+require "rishuraj.php";
+$db = new login();
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    if ($db->dbConnect()) {
+        if ($db->logIn("users", $_POST['username'], $_POST['password'])) {
+            echo "Login Success";
+        } else echo "Username or Password wrong";
+    } else echo "Error: Database connection";
+} else echo "All fields are required";
+?>
